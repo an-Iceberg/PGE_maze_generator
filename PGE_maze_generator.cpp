7cb566f240e0aa94681d44dca612d7fa930ab346
an-Iceberg <a.g.marosi@bluewin.ch>
2022-07-11 14:27:05 +0200

    finished optimisations, added top of stack drawing

--- PGE_maze_generator.cpp
+++ PGE_maze_generator.cpp
@@ -29,6 +29,7 @@ public:
   }
 };
 
+// TODO: add UI section on top with controls for delay
 class MazeGenerator : public olc::PixelGameEngine
 {
 public:
@@ -43,10 +44,12 @@ private:
   int mazeHeight; // Maze height in maze cells
   int cellCount; // Number of cells in the maze
   int pathWidth; // Path width in pixels
-  std::vector<cell> mazeDirections; // Vector containing all cells and their data/information
+  std::vector<cell> maze; // Vector containing all cells and their data/information
   // ? why is this necessary?
   int visitedCellsCounter; // Number of cells that has been visited
   std::stack<olc::vi2d> unvisitedCells; // Contains all maze cells (as coordinates) who's direction has not yet been set
+  float delay; // Delay in seconds
+  float timePassed;
 
 public:
   bool OnUserCreate() override
@@ -57,23 +60,23 @@ public:
     mazeHeight = 50;
     mazeWidth = 50;
     cellCount = mazeWidth * mazeHeight;
+    pathWidth = 3;
+    visitedCellsCounter = cellCount + 1;
+    delay = 0.000f;
 
     // Set all the maze cells to have no direction
     for (int i = 0; i < cellCount; i++)
     {
-      mazeDirections.push_back(cell());
+      maze.push_back(cell());
     }
 
-    unvisitedCells.push(olc::vi2d{0, 0});
+    Clear(olc::BLACK);
 
-    visitedCellsCounter = 1;
-
-    pathWidth = 3;
+    PaintingRoutine();
 
     return true;
   }
 
-  // TODO: user adjustable maze drawing speed delay
   bool OnUserUpdate(float fElapsedTime) override
   {
     // Generate new maze when ENTER key is pressed
@@ -84,7 +87,7 @@ public:
       visitedCellsCounter = 1;
 
       // Resetting all maze data
-      for (std::vector<cell>::iterator cell = mazeDirections.begin(); cell < mazeDirections.end(); cell++)
+      for (std::vector<cell>::iterator cell = maze.begin(); cell < maze.end(); cell++)
       {
         cell->direction = NOT_SET;
         cell->hasBeenPainted = false;
@@ -94,79 +97,73 @@ public:
       unvisitedCells.push(olc::vi2d{0, 0});
     }
 
-    // As long as there are unvisited cells, update the maze
-    if (visitedCellsCounter < cellCount)
+    timePassed += fElapsedTime;
+
+    // Only draw after a certain delay time has been reached
+    if (timePassed > delay)
     {
-      std::vector<Direction> validNeighbours;
+      // Reset delay counter
+      timePassed = 0;
 
-      // Checks if neighbours exist and if their direction has been set
-      addAllValidNeighbours(validNeighbours);
-
-      // If there are any valid neighbours choose a random one
-      if (!validNeighbours.empty())
+      // As long as there are unvisited cells, update the maze
+      if (visitedCellsCounter < cellCount)
       {
-        // Chooses a random neighbour from all valid neighbours
-        Direction nextCellDirection = validNeighbours[rand() % validNeighbours.size()];
+        std::vector<Direction> validNeighbours;
 
-        cell& currentCell = mazeDirections[IndexOfCurrentCell()];
+        // Checks if neighbours exist and if their direction has been set
+        addAllValidNeighbours(validNeighbours);
 
-        // Set the current cell's direction to point towards the selected neighbour
-        currentCell.direction = nextCellDirection;
-        currentCell.hasBeenPainted = false;
-
-        // Push the selected cell onto the stack
-        unvisitedCells.push(CoordinatesOfNeighbour(nextCellDirection));
-
-        visitedCellsCounter++;
-      }
-      // There are no valid neighbours so we need to back-track until we find some valid ones
-      else
-      {
-        // Setting the enpoint cell's direction to point to its previous cell on the stack
-        // While backtracking we reverse all the directions that have been set (dunno why but it seems to work)
-        cell& previousCell = mazeDirections[IndexOfCurrentCell()];
-
-        unvisitedCells.pop();
-
-        switch (mazeDirections[IndexOfCurrentCell()].direction)
+        // If there are any valid neighbours choose a random one
+        if (!validNeighbours.empty())
         {
-          case UP:
-            previousCell.direction = DOWN;
-          break;
+          // Chooses a random neighbour from all valid neighbours
+          Direction nextCellDirection = validNeighbours[rand() % validNeighbours.size()];
 
-          case LEFT:
-            previousCell.direction = RIGHT;
-          break;
+          cell& currentCell = maze[IndexOfCurrentCell()];
 
-          case DOWN:
-            previousCell.direction = UP;
-          break;
+          // Set the current cell's direction to point towards the selected neighbour
+          currentCell.direction = nextCellDirection;
+          currentCell.hasBeenPainted = false;
 
-          case RIGHT:
-            previousCell.direction = LEFT;
-          break;
+          // Push the selected cell onto the stack
+          unvisitedCells.push(CoordinatesOfNeighbour(nextCellDirection));
+
+          visitedCellsCounter++;
+        }
+        // There are no valid neighbours so we need to back-track until we find some valid ones
+        else
+        {
+          // Setting the enpoint cell's direction to point to its previous cell on the stack
+          // While backtracking we reverse all the directions that have been set (dunno why but it seems to work)
+          cell& previousCell = maze[IndexOfCurrentCell()];
+
+          unvisitedCells.pop();
+
+          switch (maze[IndexOfCurrentCell()].direction)
+          {
+            case UP:
+              previousCell.direction = DOWN;
+            break;
+
+            case LEFT:
+              previousCell.direction = RIGHT;
+            break;
+
+            case DOWN:
+              previousCell.direction = UP;
+            break;
+
+            case RIGHT:
+              previousCell.direction = LEFT;
+            break;
+          }
+
+          previousCell.hasBeenPainted = false;
         }
 
-        previousCell.hasBeenPainted = false;
+        PaintingRoutine();
       }
-
-      PaintingRoutine();
     }
-
-    // TODO: paint the top of the stack
-    // Draws the top of the stack
-    // olc::vi2d topOfTheStack = {unvisitedCells.top().x, unvisitedCells.top().y};
-    // for (int py = 0; py < pathWidth; py++)
-    // {
-    //   for (int px = 0; px < pathWidth; px++)
-    //   {
-    //     Draw(unvisitedCells.top().x * (pathWidth + 1) + px + 1, unvisitedCells.top().y * (pathWidth + 1) + py + 1, olc::GREEN);
-    //   }
-    // }
-
-    // TODO: fill in the last cell and don't paint the top of the stack anymore
-
-    // TODO: add user adjustable delay
 
     return true;
   }
@@ -180,12 +177,10 @@ public:
    */
   void PaintingRoutine()
   {
-    // Clear(olc::BLACK);
-
     // Draws each cell
-    for (std::vector<cell>::iterator cell = mazeDirections.begin(); cell < mazeDirections.end(); cell++)
+    for (std::vector<cell>::iterator cell = maze.begin(); cell < maze.end(); cell++)
     {
-      int currentCellIndex = std::distance(mazeDirections.begin(), cell);
+      int currentCellIndex = std::distance(maze.begin(), cell);
 
       // Calculating the x and y coordinates of the current cell
       // x = index % height
@@ -195,51 +190,92 @@ public:
       // Painting the cell only if it hasn't been painted before
       if (!cell->hasBeenPainted)
       {
-        // Paints the cell
-        for (int i = 0; i < (cell->direction == NOT_SET ? pathWidth + 1 : pathWidth); i++)
+        olc::Pixel interiorColor;
+
+        // Paints the cell interior
+        if (cell->direction == NOT_SET)
         {
-          // Paints the cell interior
-
-          // Diagonal
-          Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-
-          // Bottom triangle
-          Draw(currentCell.x + (currentCell.x * pathWidth + 0) + 1, currentCell.y + (currentCell.y * pathWidth + 1) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-          Draw(currentCell.x + (currentCell.x * pathWidth + 0) + 1, currentCell.y + (currentCell.y * pathWidth + 2) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-          Draw(currentCell.x + (currentCell.x * pathWidth + 1) + 1, currentCell.y + (currentCell.y * pathWidth + 2) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-
-          // Top triangle
-          Draw(currentCell.x + (currentCell.x * pathWidth + 1) + 1, currentCell.y + (currentCell.y * pathWidth + 0) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-          Draw(currentCell.x + (currentCell.x * pathWidth + 2) + 1, currentCell.y + (currentCell.y * pathWidth + 0) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-          Draw(currentCell.x + (currentCell.x * pathWidth + 2) + 1, currentCell.y + (currentCell.y * pathWidth + 1) + 1, (cell->direction == NOT_SET ? olc::BLUE : olc::WHITE));
-
-          // Paints the respective wall
-          switch (cell->direction)
-          {
-            case NOT_SET:
-              Draw(currentCell.x + (currentCell.x * pathWidth + pathWidth) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1, olc::BLACK);
-              Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + pathWidth) + 1, olc::BLACK);
-            break;
-
-            case UP:
-              Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth - 1) + 1);
-            break;
-
-            case LEFT:
-              Draw(currentCell.x + (currentCell.x * pathWidth - 1) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1);
-            break;
-
-            case DOWN:
-              Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + pathWidth) + 1);
-            break;
-
-            case RIGHT:
-              Draw(currentCell.x + (currentCell.x * pathWidth + pathWidth) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1);
-            break;
-          }
+          interiorColor = olc::BLUE;
+        }
+        else
+        {
+          interiorColor = olc::WHITE;
         }
 
+        paintCellInterior(currentCell, interiorColor);
+        paintCellWall(currentCell, cell->direction);
+
         cell->hasBeenPainted = true;
+      }
+
+      // If this cell is the top of the stack
+      // HACK: the -1 correction on the x is necessary for some reason
+      currentCell.x--;
+
+      if (!unvisitedCells.empty() && currentCell.x == unvisitedCells.top().x && currentCell.y == unvisitedCells.top().y)
+      {
+        // On the last painting cycle the top of the stack is painted as a regular cell
+        if (visitedCellsCounter == cellCount)
+        {
+          paintCellInterior(currentCell, olc::WHITE);
+
+          continue;
+        }
+
+        paintCellInterior(currentCell, olc::GREEN);
+
+        maze[currentCellIndex].hasBeenPainted = false;
+      }
+    }
+  }
+
+  // Paints the interior of a cell
+  void paintCellInterior(const olc::vi2d& currentCell, const olc::Pixel& interiorColor)
+  {
+    // Bottom triangle
+    Draw(currentCell.x + (currentCell.x * pathWidth + 0) + 1, currentCell.y + (currentCell.y * pathWidth + 1) + 1, interiorColor);
+    Draw(currentCell.x + (currentCell.x * pathWidth + 0) + 1, currentCell.y + (currentCell.y * pathWidth + 2) + 1, interiorColor);
+    Draw(currentCell.x + (currentCell.x * pathWidth + 1) + 1, currentCell.y + (currentCell.y * pathWidth + 2) + 1, interiorColor);
+
+    // Top triangle
+    Draw(currentCell.x + (currentCell.x * pathWidth + 1) + 1, currentCell.y + (currentCell.y * pathWidth + 0) + 1, interiorColor);
+    Draw(currentCell.x + (currentCell.x * pathWidth + 2) + 1, currentCell.y + (currentCell.y * pathWidth + 0) + 1, interiorColor);
+    Draw(currentCell.x + (currentCell.x * pathWidth + 2) + 1, currentCell.y + (currentCell.y * pathWidth + 1) + 1, interiorColor);
+
+    // Paints the cell diagonal
+    for (int i = 0; i < pathWidth; i++)
+    {
+      Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1, interiorColor);
+    }
+  }
+
+  // Paints the walls of a cell
+  void paintCellWall(const olc::vi2d& currentCell, const Direction& direction)
+  {
+    for (int i = 0; i < (direction == NOT_SET ? pathWidth + 1 : pathWidth); i++)
+    {
+      switch (direction)
+      {
+        case NOT_SET:
+          Draw(currentCell.x + (currentCell.x * pathWidth + pathWidth) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1, olc::BLACK);
+          Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + pathWidth) + 1, olc::BLACK);
+        break;
+
+        case UP:
+          Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth - 1) + 1);
+        break;
+
+        case LEFT:
+          Draw(currentCell.x + (currentCell.x * pathWidth - 1) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1);
+        break;
+
+        case DOWN:
+          Draw(currentCell.x + (currentCell.x * pathWidth + i) + 1, currentCell.y + (currentCell.y * pathWidth + pathWidth) + 1);
+        break;
+
+        case RIGHT:
+          Draw(currentCell.x + (currentCell.x * pathWidth + pathWidth) + 1, currentCell.y + (currentCell.y * pathWidth + i) + 1);
+        break;
       }
     }
   }
@@ -256,7 +292,7 @@ public:
     if (unvisitedCells.top().y > 0)
     {
       // If the upper neightbour's direction is not set add it as a valid neighbour
-      if (mazeDirections[IndexOfNeighbour(Direction{UP})].direction == NOT_SET)
+      if (maze[IndexOfNeighbour(Direction{UP})].direction == NOT_SET)
       {
         neighbours.push_back(Direction{UP});
       }
@@ -266,7 +302,7 @@ public:
     if (unvisitedCells.top().x > 0)
     {
       // If the left neightbour's direction is not set add it as a valid neighbour
-      if (mazeDirections[IndexOfNeighbour(Direction{LEFT})].direction == NOT_SET)
+      if (maze[IndexOfNeighbour(Direction{LEFT})].direction == NOT_SET)
       {
         neighbours.push_back(Direction{LEFT});
       }
@@ -276,7 +312,7 @@ public:
     if (unvisitedCells.top().y < mazeWidth - 1)
     {
       // If the lower neightbour's direction is not set add it as a valid neighbour
-      if (mazeDirections[IndexOfNeighbour(Direction{DOWN})].direction == NOT_SET)
+      if (maze[IndexOfNeighbour(Direction{DOWN})].direction == NOT_SET)
       {
         neighbours.push_back(Direction{DOWN});
       }
@@ -286,20 +322,20 @@ public:
     if (unvisitedCells.top().x < mazeHeight - 1)
     {
       // If the right neightbour's direction is not set add it as a valid neighbour
-      if (mazeDirections[IndexOfNeighbour(Direction{RIGHT})].direction == NOT_SET)
+      if (maze[IndexOfNeighbour(Direction{RIGHT})].direction == NOT_SET)
       {
         neighbours.push_back(Direction{RIGHT});
       }
     }
   }
 
-  // Returns the index of a cell's neighbour in mazeDirections
+  // Returns the index of a cell's neighbour in maze
   int IndexOfNeighbour(olc::vi2d direction)
   {
     return (unvisitedCells.top().y + direction.y) * mazeWidth + (unvisitedCells.top().x + direction.x);
   }
 
-  // Returns the index of a cell's neighbour in mazeDirections or the current cell's
+  // Returns the index of a cell's neighbour in maze or the current cell's
   int IndexOfNeighbour(Direction direction)
   {
     int index;
